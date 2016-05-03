4e3d1cee9eebf1a6d69ccab7feac7620b462ed4c
Herman Chan <hermchan89@gmail.com>
2016-05-03 16:00:48 -0700

    added users login and show controller and routes

--- lib_app2/app/controllers/users_controller.rb
+++ lib_app2/app/controllers/users_controller.rb
@@ -15,4 +15,10 @@ class UsersController < ApplicationController
     redirect_to root_path
   end
 
+  def show
+    @user = User.find_by_id(parmas[:id])
+  end
+
+  
+
 end
--- lib_app2/config/routes.rb
+++ lib_app2/config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get "/users", to:"users#index"
   get "/users/new", to:"users#new", as:"new_user"
   post "/users", to:"users#create"
+  get "/users/:id", to:"users#show", as:"user"
 
 
   # The priority is based upon order of creation: first created -> highest priority.
